9979fe8bf9093920ed2b93b1db4363ce8031ae3f
Collin Christensen <collinrchristensen@gmail.com>
2018-01-30 17:45:54 -0700

    got tilemap working in vector project

---
+++ VectorUnknown/vector_unknown/Assets/VectorUnknown/Scripts/ScrollingBackground.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ScrollingBackground : MonoBehaviour
+{
+    public Renderer renderer;
+
+    public float scrollSpeed = 0.5F;
+
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+    void Update()
+    {
+        float offset = Time.time * scrollSpeed;
+        renderer.material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+    }
+}
